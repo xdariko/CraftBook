8da8587e76738432ee3cb4a3bd2a798091b5b13a
Daria-Romanko <Diagic1000@gmail.com>
2026-02-06 16:35:12 +0300

    Fix 2

--- test/models/ingredients_tag_test.rb
+++ test/models/ingredients_tag_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class IngridientsRecipeTest < ActiveSupport::TestCase
+class IngredientsTagTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
